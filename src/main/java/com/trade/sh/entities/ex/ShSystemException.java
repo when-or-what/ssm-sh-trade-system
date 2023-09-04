1520d4b5cf567f39c03217560036291460e1114a
when-or-what <2031449330@qq.com>
2023-09-04 21:12:51 +0800

    完善，并且将数据库地址更改至测试数据库

--- src/main/java/com/trade/sh/entities/ex/ShSystemException.java
+++ src/main/java/com/trade/sh/entities/ex/ShSystemException.java
@@ -3,11 +3,13 @@ package com.trade.sh.entities.ex;
 import com.trade.sh.utils.ResultEnum;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class ShSystemException extends RuntimeException {
     private Integer state;
     private String message;
